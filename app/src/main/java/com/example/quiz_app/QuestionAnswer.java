5f1ce0e2ddfcd5d4d9389e5d4e2f2c6517e72796
Amritsks <amritsks1111@gmail.com>
2024-03-15 19:42:42 +0530

    Final commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "'Os' computer abbreviation usually means",
+            "What is the part of database that holds only one type of information?",
+            "How many bits in a byte?",
+            "Which of the following in a search engine?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"Order of Significance" , "Open Software" , "Operating System" , "Optical Sensor"},
+            {"Report" , "Field" , "Record" , "None of the above"},
+            {"9" , "4" , "10" , "8"},
+            {"Amazon" , "Google" , "Netscape" , "Wikipedia"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "Operating System" ,
+            "Field",
+            "8",
+            "Google",
+            "All of the above"};
+}
